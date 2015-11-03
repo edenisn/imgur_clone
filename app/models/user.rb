4b5fb976780bf18e4c8520828d1a38bf74807e13
Denis Erofeev <edenisn@gmail.com>
2015-11-03 23:28:30 +0300

    allow users to sign in using their username or email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,37 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :login
 
-  validates_presence_of :username
-  validates_uniqueness_of :username
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => {email: true, login: false}
+
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value",
+                               { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
