adad0393e8ee7370891c7bc8ce53c8a49bfecc60
Denis Erofeev <edenisn@gmail.com>
2015-11-04 08:26:36 +0300

    set username unique in migration and in auth_keys set email: false, username: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => {email: true, login: false}
+         authentication_keys: { email: false, login: true }
 
   validates :username,
             :presence => true,
@@ -18,12 +18,11 @@ class User < ActiveRecord::Base
       errors.add(:username, :invalid)
     end
   end
-  
+
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value",
-                               { :value => login.downcase }]).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
       if conditions[:username].nil?
         where(conditions).first
