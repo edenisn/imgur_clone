84588aa2e8494517db0f80bfb89deb9baa54b9b8
Denis Erofeev <edenisn@gmail.com>
2015-11-08 21:20:51 +0300

    add data for reports in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_image(file_name, filename_extension)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{filename_extension}/#{file_name}.#{filename_extension}"))
+end
+
+1.upto(2) do |i|
+  User.create!(email: "test#{i}@test.com", password: "12345678", password_confirmation: "12345678", username: "test#{i}")
+  1.upto(2) do |j|
+    Image.create!(name: "test#{j}", description: "test#{j}", attachment: seed_image("test#{j}", "jpg"),
+                  created_at: Time.gm(2015, "sep", "#{j}", "#{j}", "#{j}", 30, 50), user_id: i)
+    Image.create!(name: "apple#{j}", description: "apple#{j}", attachment: seed_image("apple#{j}", "png"),
+                  created_at: Time.gm(2015, "jun", "#{j}", "#{j}", "#{j}", 30, 50), user_id: i)
+    Image.create!(name: "devil#{j}", description: "devil#{j}", attachment: seed_image("devil#{j}", "gif"),
+                  created_at: Time.gm(2015, "aug", "#{j}", "#{j}", "#{j}", 30, 50), user_id: i)
+    Image.create!(name: "ruby#{j}", description: "ruby#{j}", attachment: seed_image("ruby#{j}", "jpeg"),
+                  created_at: Time.gm(2015, "feb", "#{j}", "#{j}", "#{j}", 30, 50), user_id: i)
+  end
+end
+
+
